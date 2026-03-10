1277f7a06a810b8a32dc71b4537e4b39e6dd3cfc
Ratik Gambhir <ratikgambhir2@gmail.com>
2026-03-10 08:12:18 -0500

    fixing wrangler files

--- curio-workers/curio-processor-worker/.wrangler/tmp/dev-hdUG7t/index.js
+++ curio-workers/curio-processor-worker/.wrangler/tmp/dev-hdUG7t/index.js
@@ -51,7 +51,7 @@ var jsonError = /* @__PURE__ */ __name(async (request, env, _ctx, middlewareCtx)
 }, "jsonError");
 var middleware_miniflare3_json_error_default = jsonError;
 
-// .wrangler/tmp/bundle-afcqf1/middleware-insertion-facade.js
+// .wrangler/tmp/bundle-14T9Uy/middleware-insertion-facade.js
 var __INTERNAL_WRANGLER_MIDDLEWARE__ = [
   middleware_ensure_req_body_drained_default,
   middleware_miniflare3_json_error_default
@@ -83,7 +83,7 @@ function __facade_invoke__(request, env, ctx, dispatch, finalMiddleware) {
 }
 __name(__facade_invoke__, "__facade_invoke__");
 
-// .wrangler/tmp/bundle-afcqf1/middleware-loader.entry.ts
+// .wrangler/tmp/bundle-14T9Uy/middleware-loader.entry.ts
 var __Facade_ScheduledController__ = class ___Facade_ScheduledController__ {
   constructor(scheduledTime, cron, noRetry) {
     this.scheduledTime = scheduledTime;
